8fe0a44cc6b8ead13d80663fb8535d4f52b648d1
Nick <nicholas.holt106@mod.gov.uk>
2024-03-27 12:16:31 +0000

    Update API URL and remove API key

--- .app.config.tsx
+++ .app.config.tsx
@@ -1,6 +1,7 @@
 export default {
     appName: "PushSpike",
     env: "production",
-    apiUrl: "<API_URL_HERE>",
-    apiKey: "755517ea-7667-4e89-8dd8-a24fb800d3d8",
+    apiUrl: "192.168.42.242:5287",
+    apiKey: "",
   };
+  //755517ea-7667-4e89-8dd8-a24fb800d3d8
